a4c1a658685d61b0e48163becc3432aff7f77a6c
Zoltan Kuscsik <zoltan.kuscsik@solution57.com>
2018-10-30 09:07:22 +0100

    AOSP: removed unused file

--- utils/v4l2-ctl/Android.mk
+++ utils/v4l2-ctl/Android.mk
@@ -17,7 +17,7 @@ LOCAL_C_INCLUDES := \
 LOCAL_SHARED_LIBRARIES := libstlport
 
 LOCAL_SRC_FILES := \
-    v4l2-ctl.cpp v4l2-ctl.h v4l2-ctl-common.cpp v4l2-ctl-tuner.cpp \
+    v4l2-ctl.cpp v4l2-ctl-common.cpp v4l2-ctl-tuner.cpp \
     v4l2-ctl-io.cpp v4l2-ctl-stds.cpp v4l2-ctl-vidcap.cpp v4l2-ctl-vidout.cpp \
     v4l2-ctl-overlay.cpp v4l2-ctl-vbi.cpp v4l2-ctl-selection.cpp v4l2-ctl-misc.cpp \
     v4l2-ctl-streaming.cpp v4l2-ctl-sdr.cpp v4l2-ctl-edid.cpp v4l2-ctl-modes.cpp \
